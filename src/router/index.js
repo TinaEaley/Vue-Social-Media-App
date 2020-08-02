e396cb53eef45e7e6408cc3d138acd4a65763fab
Christina Delligatta <tinadelli@MacBook-Pro.local>
2020-08-01 23:56:30 -0400

    setting up auth routes with vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
+import { auth } from "../firebase";
 
 Vue.use(VueRouter);
 
@@ -8,7 +9,10 @@ const routes = [
   {
     path: "/",
     name: "Dashboard",
-    component: Dashboard
+    component: Dashboard,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: "/login",
@@ -17,7 +21,7 @@ const routes = [
     // this generates a separate chunk (login.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "login" */ "../views/Login.vue")
+      import(/* webpackChunkName: "login" */ "../views/Login.vue"),
   },
   {
     path: "/settings",
@@ -26,14 +30,30 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "settings" */ "../views/Settings.vue")
-  }
+      import(/* webpackChunkName: "settings" */ "../views/Settings.vue"),
+    meta: {
+      requiresAuth: true,
+    },
+  },
 ];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
+});
+
+//check if the route exists and reuqires authentication
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some((x) => x.meta.requiresAuth);
+
+  //if the route requiresAuth is true and user is not logged in redirect to login
+  if (requiresAuth && !auth.currentUser) {
+    next("/login");
+  } else {
+    //else send them to the route the logged in user is trying to visit || to the route if the route doesn't reuqire auth
+    next();
+  }
 });
 
 export default router;
