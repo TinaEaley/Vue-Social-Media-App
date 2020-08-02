99b7458d52e69dfae64b42c9dbbf0cd573cc54d3
Christina Delligatta <tinadelli@MacBook-Pro.local>
2020-08-01 23:18:58 -0400

    setting up firebase

--- src/main.js
+++ src/main.js
@@ -2,11 +2,18 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { auth } from "./firebase";
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+//We need to ensure Firebase initializes before loading our app when a user refreshes a page
+let app;
+auth.onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: (h) => h(App),
+    }).$mount("#app");
+  }
+});
